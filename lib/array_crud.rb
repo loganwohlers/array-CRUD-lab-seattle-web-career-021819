514761f7fd0070f145ba0abf965c1ee115b2d79f
Logan Wohlers <lawohlers@gmail.com>
2019-02-03 23:24:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,24 +1,28 @@
 def create_an_empty_array
-  
+  a=[]  
 end
 
 def create_an_array
-  
+  a=[1,2,3,4]  
 end
 
 def add_element_to_end_of_array(array, element)
+  array.push(element)
   
 end
 
 def add_element_to_start_of_array(array, element)
+  array.unshift(element)
   
 end
 
 def remove_element_from_end_of_array(array)
+  array.pop
   
 end
 
 def remove_element_from_start_of_array(array)
+  array.shift
   
 end
 
